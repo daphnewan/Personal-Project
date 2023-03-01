8fad643d68e77cb3684f9da97f83ae7d02291630
Daphne <121971548+daphnewan@users.noreply.github.com>
2023-03-01 10:42:55 -0800

    new changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 10.0f;
+    public float speed = 10.0f;
+    private float xBound = 9;
     private Rigidbody playerRb;
-    private float xBound = 10;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,20 +17,31 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+        ConstrainPlayerPosition();
+       
+    }
+    //Moves the player based on arrow key input
+    void MovePlayer()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(Vector3.forward * speed * verticalInput);
         playerRb.AddForce(Vector3.right * speed * horizontalInput);
-
+    }
+    //Prevent the player from leaving the top or bottom of the screen
+    void ConstrainPlayerPosition()
+    {
         if (transform.position.x < -xBound)
         {
-            transform.position = new Vector3(transform.position.y, transform.position.z, -xBound);
+            transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
         }
 
         if (transform.position.x > xBound)
         {
-            transform.position = new Vector3(transform.position.y, transform.position.z, xBound);
+            transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
         }
     }
+
 }
